2d6f56f28f7eb79ace8d34f98417d332194bd7ef
Junghwan Kim <junghwki@c8r1s10.42seoul.kr>
2021-04-22 17:51:24 +0900

    210422_libasm_01

--- 06_libasm/main.c
+++ 06_libasm/main.c
@@ -12,40 +12,44 @@ ssize_t	ft_write(int fildex, void *buf, size_t nbyte);
 int	main(void)
 {
 	char a[15];
-	char *b = "Hello ";
-	char *c = "World !";
 
-	printf("\n<<< strlen >>>\n");
-	printf("lib = %zu\n", strlen(b));
-	printf("own = %zu\n", ft_strlen(b));
-	printf("\n<<< strlen >>>\n");
+	printf("\n-======<<< strlen >>>======-\n");
+	printf("lib = %zu\n", strlen("Hello "));
+	printf("own = %zu\n", ft_strlen("Hello "));
+	printf("lib = %zu\n", strlen("World!"));
+	printf("own = %zu\n", ft_strlen("World!"));
+	printf("lib = %zu\n", strlen("Hello World!"));
+	printf("own = %zu\n", ft_strlen("Hello World!"));
+	printf("-======<<< strlen >>>======-\n");
 
-	printf("\n<<< strcmp >>>\n");
-	printf("lib = %d\n", strcmp(b, c));
-	printf("own = %d\n", ft_strcmp(b, c));
-	printf("lib = %d\n", strcmp(c, b));
-	printf("own = %d\n", ft_strcmp(c, b));
-	printf("\n<<< strcmp >>>\n");
+	printf("\n-======<<< strcmp >>>======-\n");
+	printf("lib = %d\n", strcmp("Hello ", "World!"));
+	printf("own = %d\n", ft_strcmp("Hello ", "World!"));
+	printf("lib = %d\n", strcmp("Hello World!", "Hello World!"));
+	printf("own = %d\n", ft_strcmp("Hello World!", "Hello World!"));
+	printf("lib = %d\n", strcmp("World!", "Hello "));
+	printf("own = %d\n", ft_strcmp("World!", "Hello "));
+	printf("-======<<< strcmp >>>======-\n");
 
-	printf("\n<<< strdup >>>\n");
-	printf("lib = %s\n", strdup(b));
-	printf("own = %s\n", ft_strdup(b));
-	printf("lib = %s\n", strdup(c));
-	printf("own = %s\n", ft_strdup(c));
-	printf("\n<<< strdup >>>\n");
+	printf("\n-======<<< strdup >>>======-\n");
+	printf("lib = %s\n", strdup("Hello "));
+	printf("own = %s\n", ft_strdup("Hello "));
+	printf("lib = %s\n", strdup("World!"));
+	printf("own = %s\n", ft_strdup("World!"));
+	printf("-======<<< strdup >>>======-\n");
 
-	printf("\n<<< strcpy >>>\n");
-	printf("lib = %s\n", strcpy(a, b));
-	printf("lib = %s\n", ft_strcpy(a, b));
-	printf("lib = %s\n", strcpy(a, c));
-	printf("lib = %s\n", ft_strcpy(a, c));
-	printf("\n<<< strcpy >>>\n");
+	printf("\n-======<<< strcpy >>>======-\n");
+	printf("lib = %s\n", strcpy(a, "Hello "));
+	printf("lib = %s\n", ft_strcpy(a, "Hello "));
+	printf("lib = %s\n", strcpy(a, "World!"));
+	printf("lib = %s\n", ft_strcpy(a, "World!"));
+	printf("-======<<< strcpy >>>======-\n");
 	
-	printf("\n<<< write >>>\n");
+	printf("\n-======<<< write >>>======-\n");
 	write(1, "lib = Hello world!", 18);
 	ft_write(1, "\nown = Hello world!", 19);
 	printf("\nlib return = %zd\n", write(-1,"Hello \n",7));
 	printf("own return = %zd\n", ft_write(-1,"Hello \n",7));
-	printf("\n<<< write >>>\n");
+	printf("-======<<< write >>>======-\n");
 	return (0);
 }
