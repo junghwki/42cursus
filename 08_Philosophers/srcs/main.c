a593171c9c7f58197bbc441652645becfcde4d9f
김정환 <junghwki@junghwki.local>
2021-07-30 00:31:13 +0900

    norminette
    함수분할 완료

--- 08_Philosophers/srcs/main.c
+++ 08_Philosophers/srcs/main.c
@@ -25,7 +25,10 @@ t_arg	*argument_init(int argc, char **argv)
 	args->time_to_sleep = ft_atoi(argv[4]);
 	if (argc == 6)
 		args->must_eat_num = ft_atoi(argv[5]);
-	args->fork = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t) * args->philo_num);
+	else
+		args->must_eat_num = 0;
+	args->fork = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t)
+		* args->philo_num);
 	pthread_mutex_init(&args->print, NULL);
 	while (idx < args->philo_num)
 	{
@@ -35,17 +38,60 @@ t_arg	*argument_init(int argc, char **argv)
 	return (args);
 }
 
-int	main(int argc, char **argv)
+t_philo	*philo_thread_create(t_arg *args)
 {
-	t_arg		*args;
-	t_philo		*philo;
-	int			idx;
-	int			eat_cnt;
-	int			flag;
+	t_philo	*philo;
+	int		idx;
+
+	idx = 0;
+	philo = (t_philo *)malloc(sizeof(t_philo) * args->philo_num);
+	while (idx < args->philo_num)
+	{
+		philo[idx].philo_idx = idx;
+		philo[idx].args = args;
+		philo[idx].eat_cnt = 0;
+		pthread_create(&(philo[idx].t_id), NULL, (void *)philosopher, &philo[idx]);
+		pthread_detach(philo[idx].t_id);
+		idx++;
+	}
+	return (philo);
+}
+
+void monitoring(t_arg *args, t_philo *philo)
+{
+	int	idx;
+	int	flag;
+	int	eat_cnt;
 
 	idx = 0;
 	flag = 0;
 	eat_cnt = 0;
+	while (!flag)
+	{
+		idx = 0;
+		while (idx < args->philo_num)
+		{
+			if (runtime(philo[idx].last_meal) > args->time_to_die)
+			{
+				pthread_mutex_lock(&(args->print));
+				printf("%d ms %d is died\n", runtime(args->start_time), idx + 1);
+				flag = 1;
+				break;
+			}
+			if (args->must_eat_num && (philo[idx].eat_cnt <= args->must_eat_num))
+				eat_cnt = philo[idx].eat_cnt;
+			idx++;
+		}
+		if (args->must_eat_num && (eat_cnt >= args->must_eat_num))
+			break;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	t_arg		*args;
+	t_philo		*philo;
+
 	if (argc == 5 || argc == 6)
 	{
 		args = argument_init(argc, argv);
@@ -56,36 +102,8 @@ int	main(int argc, char **argv)
 			free(args);
 			return (0);
 		}
-		idx = 0;
-		philo = (t_philo *)malloc(sizeof(t_philo) * args->philo_num);
-		while (idx < args->philo_num)
-		{
-			philo[idx].philo_idx = idx;
-			philo[idx].args = args;
-			philo[idx].eat_cnt = 0;
-			pthread_create(&(philo[idx].t_id), NULL, (void *)philosopher, &philo[idx]);
-			pthread_detach(philo[idx].t_id);
-			idx++;
-		}
-		while (!flag)
-		{
-			idx = 0;
-			while (idx < args->philo_num)
-			{
-				if (runtime(philo[idx].last_meal) > args->time_to_die)
-				{
-					pthread_mutex_lock(&(args->print));
-					printf("%d ms %d is died\n", runtime(args->start_time), idx + 1);
-					flag = 1;
-					break ;
-				}
-				if ((argc == 6) && (philo[idx].eat_cnt <= args->must_eat_num))
-					eat_cnt = philo[idx].eat_cnt;
-				idx++;
-			}
-			if ((argc == 6) && (eat_cnt >= args->must_eat_num))
-				break ;
-		}
+		philo = philo_thread_create(args);
+		monitoring(args, philo);
 		free(args->fork);
 		free(args);
 	}
