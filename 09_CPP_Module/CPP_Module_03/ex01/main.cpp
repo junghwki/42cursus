e37bdc279df9aeeb60cc9c2766856b350cf8cd56
Junghwan Kim <junghwki@c5r2s5.42seoul.kr>
2021-12-06 17:46:20 +0900

    main문 수정

--- 09_CPP_Module/CPP_Module_03/ex01/main.cpp
+++ 09_CPP_Module/CPP_Module_03/ex01/main.cpp
@@ -5,25 +5,30 @@ int main(void)
 {
 	ScavTrap a("junghwki");
 	ScavTrap b(a);
-	ClapTrap c("juyang");
+	ScavTrap c;
+	ClapTrap d("juyang");
 
-
+	c = a;
 	std::cout << "================================== attack ==================================" <<std::endl;
 	a.attack("juyang");
 	b.attack("juyang");
 	c.attack("juyang");
+	d.attack("juyang");
 	std::cout << "================================ takeDamage ================================" <<std::endl;
 	a.takeDamage(10);
 	b.takeDamage(10);
 	c.takeDamage(10);
+	d.takeDamage(10);
 	std::cout << "================================ beRepaired ================================" <<std::endl;
 	a.beRepaired(10);
 	b.beRepaired(10);
 	c.beRepaired(10);
+	d.beRepaired(10);
 	std::cout << "================================ guard Gate ================================" <<std::endl;
 	a.guardGate();
 	b.guardGate();
-	// c.guardGate();
+	c.guardGate();
+	// d.guardGate();
 	std::cout << "============================================================================" <<std::endl;
 	return (0);
 }
