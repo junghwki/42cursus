3bcbbd3d408e95f882fe1d0b4a586dafc811c377
Junghwan Kim <junghwki@c5r2s5.42seoul.kr>
2021-12-15 18:36:46 +0900

    CPP_Module_07 끝

--- 09_CPP_Module/CPP_Module_07/ex01/main.cpp
+++ 09_CPP_Module/CPP_Module_07/ex01/main.cpp
@@ -3,21 +3,21 @@
 
 template <typename T>
 void showArrayElement(T param){
-    std::cout << param << std::endl;
+	std::cout << param << std::endl;
 }
 
 int main(void){
-    int arrInt[5] = {1, 2, 3, 4, 5};
-    char arrChar[5] = {'a', 'b', 'c', 'd', 'e'};
-    double arrDouble[5] = {1.1, 2.2, 3.3, 4.4, 5.5};
+	int arrInt[5] = {1, 2, 3, 4, 5};
+	char arrChar[5] = {'a', 'b', 'c', 'd', 'e'};
+	double arrDouble[5] = {1.1, 2.2, 3.3, 4.4, 5.5};
 
-    std::cout << "---------- Int ----------" << std::endl;
-    ::iter(arrInt, 5, &showArrayElement);
-    std::cout << "-------------------------" << std::endl << std::endl;
-    std::cout << "---------- Char ---------" << std::endl;
-    ::iter(arrChar, 5, &showArrayElement);
-    std::cout << "-------------------------" << std::endl << std::endl;
-    std::cout << "-------- Double ---------" << std::endl;
-    ::iter(arrDouble, 5, &showArrayElement);
-    std::cout << "-------------------------" << std::endl;
+	std::cout << "---------- Int ----------" << std::endl;
+	::iter(arrInt, 5, &showArrayElement);
+	std::cout << "-------------------------" << std::endl << std::endl;
+	std::cout << "---------- Char ---------" << std::endl;
+	::iter(arrChar, 5, &showArrayElement);
+	std::cout << "-------------------------" << std::endl << std::endl;
+	std::cout << "-------- Double ---------" << std::endl;
+	::iter(arrDouble, 5, &showArrayElement);
+	std::cout << "-------------------------" << std::endl;
 }
